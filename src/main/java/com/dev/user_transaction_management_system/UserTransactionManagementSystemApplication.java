75679904e36c7158ab49366c6f1f62fdbac413bf
amir-original <amirrahmani7017@gmail.com>
2025-04-27 09:50:33 +0330

    init project

---
+++ src/main/java/com/dev/user_transaction_management_system/UserTransactionManagementSystemApplication.java
@@ -0,0 +1,13 @@
+package com.dev.user_transaction_management_system;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class UserTransactionManagementSystemApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(UserTransactionManagementSystemApplication.class, args);
+	}
+
+}
